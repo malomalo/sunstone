ad61aa3e54915570f5cd504077ddbcde52217fb4
Jon Bracy <jonbracy@gmail.com>
2016-10-11 18:14:31 -0700

    Fix JSON type to allow rails to know when dirty

--- lib/active_record/connection_adapters/sunstone/type/json.rb
+++ lib/active_record/connection_adapters/sunstone/type/json.rb
@@ -4,6 +4,11 @@ module ActiveRecord
       module Type
         class Json < ActiveRecord::Type::Internal::AbstractJson
 
+
+          def deserialize(value)
+            value.dup
+          end
+          
           def serialize(value)
             value
           end
