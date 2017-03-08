8dcd935e4e2c614173c261c3faef4494775f3d44
Jon Bracy <jonbracy@gmail.com>
2017-03-08 09:49:24 -0800

    All test passing again

--- test/active_record/associations/has_and_belongs_to_many_test.rb
+++ test/active_record/associations/has_and_belongs_to_many_test.rb
@@ -2,9 +2,32 @@ require 'test_helper'
 
 class ActiveRecord::Associations::HasAndBelongsToManyTest < ActiveSupport::TestCase
 
+  schema do
+    create_table "ships" do |t|
+      t.string   "name",                    limit: 255
+    end
+
+    create_table "sailors" do |t|
+      t.string   "name",                    limit: 255
+    end
+
+    create_table "sailors_ships", id: false do |t|
+      t.integer  "ship_id",                 null: false
+      t.integer  "sailor_id",               null: false
+    end
+  end
+  
+  class Ship < ActiveRecord::Base
+    has_and_belongs_to_many :sailors
+  end
+
+  class Sailor < ActiveRecord::Base
+    has_and_belongs_to_many :ships
+  end
+
   test '#relation_ids' do
     webmock(:get, "/ships", where: {id: 42}, limit: 1).to_return(body: [{id: 42, name: "The Niña"}].to_json)
-    webmock(:get, "/sailors", where: {sailors_ships: {ship_id: {eq: 42}}}, limit: 100, offset: 0).to_return(body: [{id: 43, name: "Chris"}].to_json)
+    webmock(:get, "/sailors", where: {sailors_ships: {ship_id: {eq: 42}}}).to_return(body: [{id: 43, name: "Chris"}].to_json)
 
     assert_equal [43], Ship.find(42).sailor_ids
   end
