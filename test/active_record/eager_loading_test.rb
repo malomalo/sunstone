0c171881b5e0ec1f0b39d4d3b847617b5b0f75d4
Jon Bracy <jonbracy@gmail.com>
2019-01-07 16:25:44 -0800

    Add failing test to fix

--- test/active_record/eager_loading_test.rb
+++ test/active_record/eager_loading_test.rb
@@ -42,9 +42,23 @@ class ActiveRecord::EagerLoadingTest < ActiveSupport::TestCase
       id: 1, ships: [{id: 1, fleet_id: 1}]
     }].to_json)
     
-    fleets = Fleet.eager_load(:ships => :sailors)
+    fleets = Fleet.eager_load(ships: :sailors)
     assert_equal [1], fleets.map(&:id)
     assert_equal [1], fleets.first.ships.map(&:id)
   end
   
+
+  test '#eager_loads' do
+    assert_equal <<-SQL, Fleet.eager_load(ships: :sailors).limit(2).to_sql
+      SELECT DISTINCT "fleets"."id"
+      FROM "fleets"
+      LEFT OUTER JOIN "ships" ON "ships"."fleet_id" = "fleets"."id"
+      LEFT OUTER JOIN "sailors" ON "sailors"."ship_id" = "ships"."id"
+      LIMIT 2
+    SQL
+
+
+
+  end
+
 end
