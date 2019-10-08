b666bbaa355087a98485bc39f24226188339f17a
James Bracy <waratuman@gmail.com>
2019-10-07 17:27:02 -0700

    Adding test case for ordering by an attribute that is on a relationship.

--- test/active_record/query/order_test.rb
+++ test/active_record/query/order_test.rb
@@ -5,10 +5,20 @@ class ActiveRecord::OrderTest < ActiveSupport::TestCase
   schema do
     create_table "ships", limit: 100 do |t|
       t.string   "name",                    limit: 255
+      t.integer  "captain_id"
+    end
+
+    create_table "captains", limit: 100 do |t|
+      t.string   "name",                    limit: 255
     end
   end
   
   class Ship < ActiveRecord::Base
+    belongs_to :captain
+  end
+
+  class Captain < ActiveRecord::Base
+    has_one :ship
   end
 
   test '::order(COLUMN)' do
@@ -17,6 +27,14 @@ class ActiveRecord::OrderTest < ActiveSupport::TestCase
     })
 
     assert_equal 42, Ship.order(:id).first.id
+  end
+
+  test '::order(RELATION)' do
+    webmock(:get, "/ships", { include: :captain, order: [{ captain: { name: :asc } }] }).to_return({
+      body: [{id: 42, captain_id: 1, captain: { id: 1, name: 'James T. Kirk' } }].to_json
+    })
+
+    assert_equal 42, Ship.order({ captain: { name: :asc }}).first.id
   end
   
   # TODO: Uses Arel::Nodes::RandomOrdering from:
