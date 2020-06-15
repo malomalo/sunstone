aaaf2b6067c0a8b0308e6a2dbbce9a53c42fae61
Jon Bracy <jonbracy@gmail.com>
2020-06-15 13:26:39 -0500

    Remove ** warnings

--- ext/active_record/callbacks.rb
+++ ext/active_record/callbacks.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module Callbacks
     private
 
-      def create_or_update(*) #:nodoc:
+      def create_or_update(**) #:nodoc:
         if self.class.connection.is_a?(ActiveRecord::ConnectionAdapters::SunstoneAPIAdapter)
           @_already_called ||= {}
           self.class.reflect_on_all_associations.each do |r|
