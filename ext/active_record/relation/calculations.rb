55c3855435388d4c7821db717aa83e6d84eab447
Jon Bracy <jonbracy@gmail.com>
2021-02-15 10:27:24 -0600

    Update extension to AR::Relation#pluck

--- ext/active_record/relation/calculations.rb
+++ ext/active_record/relation/calculations.rb
@@ -2,30 +2,22 @@ module ActiveRecord
   module Calculations
     
     def pluck(*column_names)
-      if loaded? && all_attributes?(column_names)
+      if loaded? && (column_names.map(&:to_s) - @klass.attribute_names - @klass.attribute_aliases.keys).empty?
         return records.pluck(*column_names)
       end
-
+      
       if has_include?(column_names.first)
         relation = apply_join_dependency
         relation.pluck(*column_names)
       elsif klass.connection.is_a?(ActiveRecord::ConnectionAdapters::SunstoneAPIAdapter)
         load
-        return records.pluck(*column_names.map{|n| n.sub(/^#{klass.table_name}\./, "")})
+        return records.pluck(*column_names.map{|n| n.to_s.sub(/^#{klass.table_name}\./, "")})
       else
         klass.disallow_raw_sql!(column_names)
-        columns = arel_columns(column_names)
         relation = spawn
-        relation.select_values = columns
-        
-        result = skip_query_cache_if_necessary do
-          if where_clause.contradiction?
-            ActiveRecord::Result.new([], [])
-          else
-            klass.connection.select_all(relation.arel, nil)
-          end
-        end
-        type_cast_pluck_values(result, columns)
+        relation.select_values = column_names
+        result = skip_query_cache_if_necessary { klass.connection.select_all(relation.arel, nil) }
+        result.cast_values(klass.attribute_types)
       end
     end
 
