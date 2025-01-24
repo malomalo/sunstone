ad2d76c8b5ab53a28c677782ad442d2110b32026
Jon Bracy <jonbracy@gmail.com>
2025-01-24 16:03:13 -0600

    Remove unnecessary code

--- ext/active_record/relation/query_methods.rb
+++ ext/active_record/relation/query_methods.rb
@@ -7,29 +7,5 @@ module ActiveRecord
       raise UnmodifiableRelation if @arel && !model.connection.is_a?(ActiveRecord::ConnectionAdapters::SunstoneAPIAdapter)
     end
 
-    # I'm not sure why but the .to_s was introduced by this
-    # PR: https://github.com/rails/rails/pull/53625
-    # which break sunstone which needs the hash to pass through
-    def arel_column(field)
-      field = field.name if is_symbol = field.is_a?(Symbol)
-
-      field = model.attribute_aliases[field] || (field.is_a?(Hash) ? field : field.to_s)
-      from = from_clause.name || from_clause.value
-
-      if field.is_a?(Hash)
-        field
-      elsif model.columns_hash.key?(field) && (!from || table_name_matches?(from))
-        table[field]
-      elsif /\A(?<table>(?:\w+\.)?\w+)\.(?<column>\w+)\z/ =~ field
-        arel_column_with_table(table, column)
-      elsif block_given?
-        yield field
-      elsif Arel.arel_node?(field)
-        field
-      else
-        Arel.sql(is_symbol ? model.adapter_class.quote_table_name(field) : field)
-      end
-    end
-    
   end
 end
